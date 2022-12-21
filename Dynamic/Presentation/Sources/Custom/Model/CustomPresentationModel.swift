db5e774fd1cbba15813d01fd080002aaf6d0a301
ornwoo96 <dongou705@naver.com>
2022-12-21 14:46:58 +0900

    feat: customViewModel Model 생성

---
+++ Dynamic/Presentation/Sources/Custom/Model/CustomPresentationModel.swift
@@ -0,0 +1,63 @@
+//
+//  CustomPresentationModel.swift
+//  DynamicPresentation
+//
+//  Created by 김동우 on 2022/12/21.
+//
+
+import Foundation
+import DynamicDomain
+
+public struct CustomPresentationModel {
+    
+    var previewImageData: [PresentationPreview]
+    var originalImageData: [PresentationOriginal]
+    
+    init(previewImageData: [PresentationPreview],
+         originalImageData: [PresentationOriginal]) {
+        self.previewImageData = previewImageData
+        self.originalImageData = originalImageData
+    }
+    
+    public struct PresentationPreview {
+        let id: String
+        let height: CGFloat
+        let width: CGFloat
+        let url: String
+        let favorite: Bool
+        
+        init(height: String,
+             width: String,
+             url: String,
+             id: String,
+             favorite: Bool = false) {
+            self.height = CGFloat(Int(height) ?? 0)
+            self.width = CGFloat(Int(width) ?? 0)
+            self.url = url
+            self.id = id
+            self.favorite = favorite
+        }
+    }
+    
+    public struct PresentationOriginal {
+        let id: String
+        let height: CGFloat
+        let width: CGFloat
+        let url: String
+        let favorite: Bool
+        
+        init(height: String,
+             width: String,
+             url: String,
+             id: String,
+             favorite: Bool = false) {
+            self.height = CGFloat(Int(height) ?? 0)
+            self.width = CGFloat(Int(width) ?? 0)
+            self.url = url
+            self.id = id
+            self.favorite = favorite
+        }
+    }
+
+    
+}
