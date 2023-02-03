702487d455b5904f357476d8d512032815d805e4
ornwoo96 <dongou705@naver.com>
2023-02-03 15:42:27 +0900

    chore: DynamicGIF framework 생성

---
+++ Dynamic/DynamicGIF/DynamicGIF/DynamicGIF.h
@@ -0,0 +1,18 @@
+//
+//  DynamicGIF.h
+//  DynamicGIF
+//
+//  Created by 김동우 on 2023/02/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicGIF.
+FOUNDATION_EXPORT double DynamicGIFVersionNumber;
+
+//! Project version string for DynamicGIF.
+FOUNDATION_EXPORT const unsigned char DynamicGIFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicGIF/PublicHeader.h>
+
+
